9a0709b9a35011ca0afee093c94709787fc8b5e0
unknown <g2431061@m.sojo-u.ac.jp>
2026-07-19 22:08:20 +0900

    first commit

---
+++ src/linear_regression.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/wine.csv')
+X = df['alcohol'].values.reshape(-1,1)
+y = df['quality'].values
+
+X_b = np.hstack([np.ones_like(X),X])
+
+beta,residuals,rank,s = np.linalg.lstsq(X_b,y,rcond=None)
+
+y_pred = X_b @ beta
+
+plt.scatter(X,y,label='Data points')
+plt.plot(X,y_pred,color='red',label='Fitted line')
+plt.xlabel('alcohol')
+plt.ylabel('quality')
+plt.legend()
+plt.show()
